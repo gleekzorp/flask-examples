6dfb60c6cbcd90b738b7680db896de4b5c25aa5c
Daniel Floyd <gleek.dan@gmail.com>
2020-11-16 11:22:30 -0700

    first portion finished

--- app.py
+++ app.py
@@ -7,14 +7,5 @@ def home():
   return render_template('index.html', name="Daniel")
 
 
-@app.route('/slug/<id>')
-def slug(id):
-  return render_template('slug.html', id=id)
-
-@app.route('/loops')
-def loops():
-  return render_template('loops.html', content=['Cory', 'Zac', 'Jayden', 'Boli', 'Evalyn'])
-
-
 if __name__ == "__main__":
   app.run(debug=True)
